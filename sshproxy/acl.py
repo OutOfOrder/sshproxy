f02f3e9b3cdbfd9023f04b149d3c6735ae72edd5
David Guerizec <david@guerizec.net>
2006-07-06 03:47:48 +0200

    ACL engine is getting a better parser.
    But the code has become uglier...

--- sshproxy/acl.py
+++ sshproxy/acl.py
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2005-2006 David Guerizec <david@guerizec.net>
 #
-# Last modified: 2006 Jul 05, 03:40:15 by david
+# Last modified: 2006 Jul 06, 03:45:29 by david
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -20,6 +20,9 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
 from registry import Registry
+
+class ParseError(Exception):
+    pass
 
 class ACLTags(Registry):
     def __init__(self, tags=None, obj=None):
@@ -56,6 +59,10 @@ class Operator(object):
         if op in Operator._ops:
             return Operator._ops[op]
         return None
+
+    @staticmethod
+    def list():
+        return [ op.token for op in self._ops ]
 
     def __str__(self):
         return '%s %s %s' % (repr(self.left), self.token, repr(self.right))
@@ -121,7 +128,7 @@ class Or(Operator):
     token = 'or'
     p = 1
     def op(self):
-        return self.left == self.right
+        return self.left or self.right
 
 class Starts(Operator):
     token = ':='
@@ -149,15 +156,15 @@ class Literal(object):
     def __repr__(self):
         if self.item is None:
             return 'L(%s)' % repr('')
-        return 'L(%s)' % repr(self.item)
+        return '%s' % repr(self.item)
 
 class Group(object):
     token = '*'
     p = -2
-    def __init__(self, items):
-        if len(items) == 3:
-            self.left, self.op, self.right = items
-        self.items = items
+    def __init__(self, left, op, right):
+        if op.token in ('.', '*'):
+            raise ParseError('Unknown operator: %s' % repr(op))
+        self.left, self.op, self.right = left, op, right
 
     def __getitem__(self, item):
         return self.items[item]
@@ -166,13 +173,14 @@ class Group(object):
         return len(self.items)
 
     def __str__(self):
-        return repr(self)
+        return ' ( %s %s %s ) ' % (str(self.left),
+                                   str(self.op),
+                                   str(self.right))
 
     def __repr__(self):
-        if len(self.items) == 3:
-            return 'G(%s %s %s)' % (repr(self.left), repr(self.op), repr(self.right))
-        else:
-            return 'G(%s)' % ' '.join([repr(o) for o in self.items])
+        return ' G( %s %s %s ) ' % (repr(self.left),
+                                   repr(self.op),
+                                   repr(self.right))
 
 
 
@@ -184,103 +192,169 @@ for cls in (Equals, Different, Superior, Inferior, SuperiorEq,
 class AccessRule(Registry):
     _class_id = 'AccessRule'
 
-    def __init__(self, *args):
-        """
-        args = ('site.group', '=', 'BSD_hosts', 'and',
-                'user.profile', '!=', 'users')
-        args = "|site.group|=|BSD_hosts|and|user.profile|!=|users|"
-        """
-        if len(args) == 1 and isinstance(args[0], str):
-            if args[0][0] == '|':
-                # XXX: naive parsing
-                args = [ e for e in args[0].split('|') if e ]
+    def __init__(self, rule):
+        tokens = self.tokenize(rule)
+
+        self.tokens = list(tokens)
+        self.rule = ' '.join(tokens)
+
+        self.tree = self.parse(self.tokens)
+
+    def _search_tok(self, s, i, *toks):
+        indexes = [len(s)]
+        s = s[i:]
+        for tok in toks:
+            if tok in s:
+                indexes.append(s.index(tok))
+        return i + min(indexes)
+
+    def tokenize(self, s):
+        i = 0
+        l = 0
+        tokens = []
+        seps = []
+        seps += ['>=', '<=', '!=', ':=', '=:']
+        seps += ['=', '>', '<']
+        seps += [' ', '\t', '\n', '\r', '"']
+        seps += ['(', ')']
+        # I don't know why the sort function does not work...
+        #def sort_seps(x, y):
+        #    r = len(x) > len(y)
+        #    return r
+        #seps.sort(cmp=sort_seps)
+        while i < len(s):
+            j = i
+            i = self._search_tok(s, i, *seps)
+            if i >= len(s):
+                break
+            if s[i] in (' ', '\t', '\n', '\r'):
+                if j < i:
+                    tokens.append(s[j:i])
+            elif s[i] == '"':
+                if j < i:
+                    raise ParseError('Missing separator: ...%s...'
+                                                        % s[max(i-5, 0):i+5])
+                i += 1
+                j = i
+                while True:
+                    i = self._search_tok(s, i, '"', '\\"')#, '\\\\')
+                    if i >= len(s):
+                        raise ParseError('Unterminated quoted string')
+                    if s[i:i+2] != '\\"':
+                        break
+                    else:
+                        i += 2
+                if j <= i < len(s):
+                    if s[i] not in seps:
+                        raise ParseError('Missing separator: ...%s...'
+                                                        % s[max(i-5, 0):i+5])
+                    tokens.append(s[j:i].replace('\\"', '"'))
             else:
-                # TODO: implement better parsing with quotes
-                pass
-
-        self.elements = list(args)
-
-        self.tree = self.parse(self.elements, [])
-
-    def parse(self, args, tree):
+                if j < i:
+                    tokens.append(s[j:i])
+                for t in seps:
+                    lt = len(t)
+                    if s[i:i+lt] == t:
+                        tokens.append(s[i:i+lt])
+                        i += lt - 1
+                        j = i
+                        break
+            i += 1
+        #tokens.append(s[j:])
+        return tokens
+            
+    def parse(self, tokens):
         i = 0
 
-        while i < len(args):
-            op = Operator.get(args[i])
+        while i < len(tokens):
+            op = Operator.get(tokens[i])
             if op:
-                args[i] = op
+                tokens[i] = op
+                # exception for not that does take only one argument
                 if op.token == 'not':
-                    args.insert(i, Literal('NULL'))
+                    tokens.insert(i, Literal('NULL'))
                     i += 1
-            elif args[i] in ('(', ')'):
+            elif tokens[i] in ('(', ')'):
                 pass
             else:
-                args[i] = Literal(args[i])
+                tokens[i] = Literal(tokens[i])
             i += 1
 
-        args = self.hier_par(args)
-        return self.hier_op(args)
+        # treat parenthesis first
+        tokens = self.hier_par(tokens)
+        # make a tree with operators
+        return self.hier_op(tokens)
 
-    def hier_par(self, args):
+    def hier_par(self, tokens, i=0, l=0):
         val = []
-        i = 0
-        while i < len(args):
-            if args[i] == '(':
-                v = self.hier_par(args[i+1:])
+        lt = len(tokens)
+        while i < lt:
+            if str(tokens[i]) == '(':
+                v, j = self.hier_par(tokens, i+1, l+1)
                 val.append(v)
-                i+= len(v)
-            elif args[i] == ')':
-                return Group(val)
+                i = j
+            elif str(tokens[i]) == ')':
+                return val, i
             else:
-                val.append(args[i])
+                val.append(tokens[i])
             i += 1
-        return Group(val)
+        return val
 
-    def hier_op(self, args):
-        if len(args) <= 3:
-            if isinstance(args, Group):
-                return args 
-            elif len(args) == 1:
-                return args[0]
-            else:
-                return Group(args)
+    def _find_center_op(self, tokens):
         max = -1
-        for i in range(len(args)):
-            if max < args[i].p:
-                max = args[i].p
+        maxi = 1
+        for i in range(len(tokens)):
+            if isinstance(tokens[i], list):
+                tokens[i] = self.hier_op(tokens[i])
+                continue
+            if max < tokens[i].p:
+                max = tokens[i].p
                 maxi = i
+        if maxi < len(tokens):
+            return [tokens[:maxi], tokens[maxi], tokens[maxi+1:]]
+        else:
+            return tokens[0]
 
-        left = None
-        if maxi >= 0:
-            left = args[:maxi]
+    def hier_op(self, tokens):
+        all = self._find_center_op(tokens)
+        if isinstance(all, list) and len(all) == 3:
+            left, op, right = all
+        else:
+            return all
 
-        right = None
-        if maxi < len(args) - 1:
-            right = args[maxi+1:]
+        if len(left) == 1:
+            left = left[0]
+        else:
+            left = self.hier_op(left)
+        if len(right) == 1:
+            right = right[0]
+        else:
+            right = self.hier_op(right)
 
-        return Group([ self.hier_op(left), args[maxi], self.hier_op(right) ])
+        ret = Group(left, op, right)
+        return ret
 
 
     def eval(self, namespace):
-        return self.eval_r(self.tree, namespace)
+        return bool(self.eval_r(self.tree, namespace))
 
     def eval_r(self, tree, namespace):
-        if isinstance(tree, Group):
-            #print tree, tree.op.token
+        if isinstance(tree, Group): # and len(tree) == 3:
             left, right = (self.eval_r(tree.left, namespace),
                            self.eval_r(tree.right, namespace))
             op = tree.op
-            #print 'left=', left
-            #print 'right=', left
-            #print 'op=', op, op.__class__
             o = op(left, right)
             print o.op(), '=', o
-            return o.op()
+            return bool(o.op())
         else:
             tree = str(tree)
             for k in namespace:
                 if tree.startswith(k+'.'):
-                    tree = getattr(namespace[k], tree[len(k)+1:])
+                    attr = tree[len(k)+1:]
+                    if hasattr(namespace[k], attr):
+                        tree = getattr(namespace[k], attr)
+                    else:
+                        tree = ''
             return tree
 
 
@@ -343,19 +417,56 @@ class AccessControl(Registry):
 
 
 if __name__ == '__main__':
-    # example usage: the 3 following statements are equivalent
-    ar = AccessRule("|site.group|=|BSD_hosts|and|not|(|user.profile|:=|users|)|")
-    ar = AccessRule('site.group', '=', 'BSD_hosts', 'and',
-        'not', '(', 'user.profile', ':=', 'users', ')')
-    ar = AccessRule('site.group', '=', 'BSD_hosts', 'and',
-        'not', 'user.profile', ':=', 'users')
-    print ar.tree
-    class A: pass
-    namespace = {'site': A(), 'user': A(), }
-    namespace['site'].group = 'BSD_hosts'
-    namespace['user'].profile = 'users_noobs'
+    import sys, datetime, time
+    # example usage:
+    arstr = '''
+        ((site.group in "BSD hosts"))
+        and((site.stime<=proxy.time)
+        and(site.etime>=proxy.time))
+        and(site.stime<=proxy.time
+        and site.etime>=proxy.time)
+        and not("noobs"=:user.profile)
+        '''
+    arstr = '''
+        (site.group in "BSD hosts"
+        and(site.stime<=proxy.time))
+        '''
+    try:
+        ar = AccessRule(arstr)
+    except ParseError, msg:
+        print 'Error:', msg
+        sys.exit(1)
+    #print repr(ar.tree)
+    class A(dict):
+        def __init__(self, **kw):
+            for k in kw:
+                setattr(self, k, str(kw[k]))
+                self[k] = str(kw[k])
+
+        def __setattr__(self, k, v):
+            dict.__setattr__(self, k, v)
+            self[k] = v
+            
+
+    namespace = {
+            'site': A(group='BSD', stime='08:00', etime='18:00'),
+            'user': A(profile='users_noobs'),
+            'client': A(username='david'),
+            'proxy': A(time=time.strftime('%H:%M')),
+            }
+
+
+    print ar.rule
+    namespace['proxy'].time = '07:00'
+    print namespace
+    print ar.eval(namespace) # prints False
+    namespace['proxy'].time = '10:00'
+    print namespace
     print ar.eval(namespace) # prints False
     namespace['user'].profile = 'admin'
+    print namespace
     print ar.eval(namespace) # prints True
-
+    namespace['proxy'].time = '07:00'
+    print namespace
+    print ar.eval(namespace) # prints False
 
