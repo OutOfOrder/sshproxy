021cb1e9830d4b1a520844b10c36250b55442c07
David Guerizec <david@guerizec.net>
2006-07-23 15:49:55 +0200

    Changed the ACL parser in favor of a modified PLY (to make it recursive).

--- sshproxy/acl.py
+++ sshproxy/acl.py
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2005-2006 David Guerizec <david@guerizec.net>
 #
-# Last modified: 2006 Jul 19, 00:25:03 by david
+# Last modified: 2006 Jul 23, 15:38:07 by david
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -23,223 +23,7 @@ import time, datetime
 
 from registry import Registry
 import log
-
-class ParseError(Exception):
-    pass
-
-
-class Operator(object):
-    _ops = {}
-    is_func = False
-    p = 0
-    def __init__(self, left, right):
-        self.right = right
-        self.left = left
-        self.__str__ = Operator.__str__
-        self.__repr__ = Operator.__repr__
-
-    @staticmethod
-    def add(cls):
-        Operator._ops[cls.token] = cls
-
-    @staticmethod
-    def get(op):
-        if op in Operator._ops:
-            return Operator._ops[op]
-        return None
-
-    @staticmethod
-    def list():
-        return [ op.token for op in self._ops ]
-
-    def __str__(self):
-        return '%s %s %s' % (repr(self.left), self.token, repr(self.right))
-
-    def __repr__(self):
-        return '%s %s %s' % (repr(self.left), self.token, repr(self.right))
-
-class Equals(Operator):
-    token = '='
-    p = 5
-    def op(self):
-        return self.left == self.right
-
-class Different(Operator):
-    token = '!='
-    p = 5
-    def op(self):
-        return self.left != self.right
-
-class Superior(Operator):
-    token = '>'
-    p = 5
-    def op(self):
-        return self.left > self.right
-
-class Inferior(Operator):
-    token = '<'
-    p = 5
-    def op(self):
-        return self.left < self.right
-
-class SuperiorEq(Operator):
-    token = '>='
-    p = 5
-    def op(self):
-        return self.left >= self.right
-
-class InferiorEq(Operator):
-    token = '<='
-    p = 5
-    def op(self):
-        return self.left <= self.right
-
-class In(Operator):
-    token = 'in'
-    p = 5
-    def op(self):
-        try:
-            return self.left in self.right
-        except TypeError:
-            log.warning("Not an iterable: %s" % repr(self.right))
-            return False
-
-class And(Operator):
-    token = 'and'
-    p = 9
-    def op(self):
-        return self.left and self.right
-
-class Or(Operator):
-    token = 'or'
-    p = 9
-    def op(self):
-        return self.left or self.right
-
-class Starts(Operator):
-    token = ':='
-    p = 5
-    def op(self):
-        return str(self.left).startswith(str(self.right))
-
-class Ends(Operator):
-    token = '=:'
-    p = 5
-    def op(self):
-        return str(self.left).endswith(str(self.right))
-
-class Function(Operator):
-    is_func = True
-    p = 4
-    def __str__(self):
-        return '%s(%s)' % (self.token, repr(self.right))
-
-    def __repr__(self):
-        return '%s(%s)' % (self.token, repr(self.right))
-
-class Not(Function):
-    token = 'not'
-    def call(self, namespace):
-        return not self.left.eval(namespace, self.right)
-
-class Int(Function):
-    token = 'int'
-    def call(self, namespace):
-        result = self.left.eval(namespace, self.right)
-        try:
-            result = int(result)
-            return result
-        except ValueError:
-            return False
-
-class List(Function):
-    token = 'list'
-    def call(self, namespace):
-        items = self.left.eval(namespace, self.right)
-        if hasattr(items, 'split'):
-            return items.split()
-        else:
-            log.warning("Not a list: %s" % repr(self.right))
-            return False
-
-class Acl(Function):
-    token = 'acl'
-    def call(self, namespace):
-        acl = self.left.eval(namespace, self.right)
-        return self.left.eval(namespace,
-                              ACLDB().check(acl, **namespace))
-
-class Literal(str):
-    token = 's'
-    p = 0
-    def __init__(self, item):
-        str.__init__(self, item)
-
-class Token(str):
-    token = '1'
-    p = 0
-    def __init__(self, item):
-        str.__init__(self, item)
-
-
-    def __repr__(self):
-        return str(self)
-
-class Const(object):
-    token = 'A'
-    p = 0
-    _constants = {
-            'True': True,
-            'False': False,
-            }
-
-    def __init__(self, item):
-        self.item = self.get_constant(item)
-
-    @staticmethod
-    def get_int(i):
-        try:
-            return int(i)
-        except ValueError:
-            return None
-
-    @classmethod
-    def get_constant(cls, token):
-        if token in cls._constants.keys():
-            return cls._constants[token]
-        return cls.get_int(token)
-
-    @classmethod
-    def is_constant(cls, token):
-        if token in cls._constants.keys():
-            return True
-        return cls.get_int(token) is not None
-
-
-
-class Group(object):
-    token = '3'
-    p = -2
-    def __init__(self, left, op, right):
-        if op.token in ('.', '*'):
-            raise ParseError('Unknown operator: %s' % repr(op))
-        self.left, self.op, self.right = left, op, right
-
-    def __str__(self):
-        return ' ( %s %s %s ) ' % (str(self.left),
-                                   str(self.op),
-                                   str(self.right))
-
-    def __repr__(self):
-        return ' G( %s %s %s ) ' % (repr(self.left),
-                                   repr(self.op),
-                                   repr(self.right))
-
-
-
-for cls in (Equals, Different, Superior, Inferior, SuperiorEq,
-            InferiorEq, In, And, Or, Starts, Ends, Not, Int, List, Acl):
-    Operator.add(cls)
+from aclparser import ACLRuleParser
 
 
 class ACLRule(Registry):
@@ -247,230 +31,56 @@ class ACLRule(Registry):
 
     def __reginit__(self, name, rule):
         self.name = name
-        tokens = self.tokenize('( %s )' % rule)
+        self.rule = rule
+        self.parser = ACLRuleParser()
 
-        self.tokens = list(tokens)
-        rule = []
-        for token in tokens:
-            if isinstance(token, Literal):
-                token = '"%s"' % token
-            rule.append(token)
-
-        self.rule = ' '.join(rule)
-
-        self.tree = self.parse(self.tokens)
-
-    #def __repr__(self):
-    #    return '[repr] %s: %s' % (self.name, self.rule)
-
-    def _search_tok(self, s, i, *toks):
-        indexes = [len(s)]
-        s = s[i:]
-        for tok in toks:
-            if tok in s:
-                indexes.append(s.index(tok))
-        return i + min(indexes)
-
-    def tokenize(self, s):
-        i = 0
-        l = 0
-        tokens = []
-        seps = []
-        seps += [' ', '\t', '\n', '\r']
-        seps += ['(', ')', '"']
-        seps += ['=', '!=']
-        seps += ['>', '>=', '<', '<=']
-        seps += [':=', '=:']
-        seps.sort(cmp=lambda x, y: cmp(len(y), len(x)))
-        while i < len(s):
-            j = i
-            i = self._search_tok(s, i, *seps)
-            if i >= len(s):
-                break
-            if s[i] in (' ', '\t', '\n', '\r'):
-                if j < i:
-                    tokens.append(s[j:i])
-            elif s[i] == '"':
-                if j < i:
-                    raise ParseError('Missing separator: ...%s...'
-                                                        % s[max(i-5, 0):i+5])
-                i += 1
-                j = i
-                while True:
-                    i = self._search_tok(s, i, '"', '\\"')#, '\\\\')
-                    if i >= len(s):
-                        raise ParseError('Unterminated quoted string')
-                    if s[i:i+2] != '\\"':
-                        break
-                    else:
-                        i += 2
-                if j <= i < len(s):
-                    if s[i] not in seps:
-                        raise ParseError('Missing separator: ...%s...'
-                                                        % s[max(i-5, 0):i+5])
-                    tokens.append(Literal(s[j:i].replace('\\"', '"')))
-            else:
-                if j < i:
-                    tokens.append(s[j:i])
-                for t in seps:
-                    lt = len(t)
-                    if s[i:i+lt] == t:
-                        tokens.append(s[i:i+lt])
-                        i += lt - 1
-                        j = i
-                        break
-            i += 1
-        #tokens.append(s[j:])
-        return tokens
-            
-    def parse(self, tokens):
-        i = 0
-
-        while i < len(tokens):
-            if not isinstance(tokens[i], Literal):
-                op = Operator.get(tokens[i])
-                if op:
-                    tokens[i] = op
-                    # exception for functions that does take only one argument
-                    if op.is_func:
-                        tokens.insert(i, Token('NULL'))
-                        i += 1
-                elif tokens[i] in ('(', ')'):
-                    pass
-                elif Const.is_constant(tokens[i]):
-                    tokens[i] = Const(tokens[i])
-                else:
-                    tokens[i] = Token(tokens[i])
-            i += 1
-
-        # treat parenthesis first
-        tokens = self.hier_par(tokens)
-        # make a tree with operators
-        return self.hier_op(tokens)
-
-    def hier_par(self, tokens, i=0, l=0):
-        val = []
-        lt = len(tokens)
-        while i < lt:
-            if str(tokens[i]) == '(':
-                try:
-                    v, j = self.hier_par(tokens, i+1, l+1)
-                except ValueError:
-                    raise ParseError('Parenthesis count mismatch in rule %s:\n'
-                                                '%s' % (self.name, self.rule))
-
-                val.append(v)
-                i = j
-            elif str(tokens[i]) == ')':
-                return val, i
-            else:
-                val.append(tokens[i])
-            i += 1
-        return val
-
-    def _find_center_op(self, tokens):
-        max = 1
-        maxi = 1
-        for i in range(len(tokens)):
-            if isinstance(tokens[i], list):
-                tokens[i] = self.hier_op(tokens[i])
-                continue
-            if max < tokens[i].p:
-                max = tokens[i].p
-                maxi = i
-        if maxi < len(tokens):
-            return [tokens[:maxi], tokens[maxi], tokens[maxi+1:]]
-        else:
-            try:
-                return tokens[0]
-            except IndexError:
-                raise ParseError("Parse error, check your ACL rules.")
-
-    def hier_op(self, tokens):
-        all = self._find_center_op(tokens)
-        if isinstance(all, list) and len(all) == 3:
-            left, op, right = all
-        else:
-            return all
-
-        if len(left) == 1:
-            left = left[0]
-        else:
-            left = self.hier_op(left)
-        if len(right) == 1:
-            right = right[0]
-        else:
-            right = self.hier_op(right)
-
-        ret = Group(left, op, right)
-        return ret
-
-
-    def eval(self, namespace, tree=None):
-        if tree is None:
-            tree = self.tree
-        return self.eval_r(tree, namespace)
-
-    def eval_r(self, tree, namespace):
-        if isinstance(tree, Group): # and len(tree) == 3:
-            op = tree.op
-            if op.is_func:
-                left = self
-                right = tree.right
-            else:
-                left, right = (self.eval_r(tree.left, namespace),
-                               self.eval_r(tree.right, namespace))
-            o = op(left, right)
-            if op.is_func:
-                result = o.call(namespace)
-            else:
-                result = o.op()
-            log.debug('ACLrule %s: %s = %s' % (self.name, result, repr(o)))
-            return result
-        elif isinstance(tree, Const):
-            return tree.item
-        elif isinstance(tree, Token):
-            if tree[:6] == 'proxy.':
-                value = self.proxy_dyn_namespace(tree[6:], namespace)
-                if value is not None:
-                    return value
-            for k in namespace.keys():
-                if tree.startswith(k+'.'):
-                    attr = tree[len(k)+1:]
-                    if namespace[k].has_key(attr):
-                        item = namespace[k][attr]
-                        if isinstance(item, ACLRule):
-                            return namespace[k][attr]
-                        else:
-                            return namespace[k][attr]
-                    else:
-                        return False
-            raise ParseError('Unknown identifier or missing quotes around %s.'
-                                                                        % tree)
-        else:
-            return tree
-
-    def proxy_dyn_namespace(self, tag, namespace):
-        if tag == 'time':
-            return str(datetime.datetime.now().strftime("%H:%M"))
-        elif tag == 'date':
-            return str(datetime.datetime.now().strftime("%Y-%m-%d"))
-        elif tag == 'datetime':
-            return str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-        elif tag == 'doy':
-            return datetime.datetime.now().strftime("%j")
-        elif tag == 'week':
-            return str(datetime.datetime.now().strftime("%W"))
-        elif tag == 'unixtime':
-            return int(time.time())
-        elif tag == 'dow':
-            return str(datetime.datetime.now().strftime("%w"))
-        elif tag == 'test':
-            return "test"
-        return None
-
+    def eval(self, namespace):
+        self.parser.namespace = namespace
+        return self.parser.eval(self.rule)
 
 ACLRule.register()
+
+class ProxyNamespace(Registry, dict):
+    _class_id = 'ProxyNamespace'
+    def __reginit__(self, default_namespace=None, **kw):
+        self.defaults = default_namespace or {}
+        dict.__init__(self, **kw)
+
+    def get(self, tag, default=None):
+        if hasattr(self, 'dot_'+tag):
+            return getattr(self, 'dot_'+tag)()
+        else:
+            return self.defaults.get(tag, default)
+
+    def __getitem__(self, tag):
+        if hasattr(self, 'dot_'+tag):
+            return getattr(self, 'dot_'+tag)()
+        raise AttributeError('proxy does not have such element: %s' % tag)
+
+    def dot_time(self):
+        return str(datetime.datetime.now().strftime("%H:%M"))
+
+    def dot_date(self):
+        return str(datetime.datetime.now().strftime("%Y-%m-%d"))
+
+    def dot_datetime(self):
+        return str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+
+    def dot_doy(self):
+        return datetime.datetime.now().strftime("%j")
+
+    def dot_week(self):
+        return str(datetime.datetime.now().strftime("%W"))
+
+    def dot_unixtime(self):
+        return int(time.time())
+
+    def dot_dow(self):
+        return str(datetime.datetime.now().strftime("%w"))
+
+ProxyNamespace.register()
+
+
 
 
 class ACLTags(Registry):
@@ -565,6 +175,7 @@ class ACLDB(Registry):
     
             result = None
             match = ''
+
             if isinstance(acl, ACLRule):
                 match = repr(acl.rule)
                 result = acl.eval(namespace)
---
+++ sshproxy/aclparser.py
@@ -0,0 +1,403 @@
+#!/usr/bin/env python
+
+import sys
+import readline
+import lex
+import ryacc
+import os
+from copy import copy
+
+class Context(object):
+    pass
+
+class Parser(object):
+    """
+    Base class for a lexer/parser that has the rules defined as methods
+    """
+    tokens = ()
+    precedence = ()
+
+
+    def __init__(self, **kw):
+        self.debug = kw.get('debug', 0)
+        self.parse_result = None # undefined
+        try:
+            modname = os.path.split(os.path.splitext(__file__)[0])[1] \
+                                + "_" + self.__class__.__name__
+        except:
+            modname = "parser"+"_"+self.__class__.__name__
+        self.debugfile = modname + ".dbg"
+        self.tabmodule = modname + "_" + "parsetab"
+        #print self.debugfile, self.tabmodule
+
+        # Build the lexer and parser
+        self.ctx = Context()
+        ryacc.ctx = self.ctx
+        self.lexer = lex.lex(module=self, debug=self.debug)
+        self.parser = ryacc.yacc(module=self,
+                  debug=self.debug,
+                  debugfile=self.debugfile,
+                  tabmodule=self.tabmodule)
+
+    def run(self):
+        while 1:
+            try:
+                s = raw_input('acl > ')
+            except EOFError:
+                break
+            if not s: continue     
+            if self.debug:
+                self.lexer.input(s)
+                while True:
+                    tok = self.lexer.token()
+                    if not tok: break
+                    print tok
+            print self.eval(s)
+
+    def eval(self, s):
+        if self.debug:
+            self.lexer.input(s)
+            while True:
+                tok = self.lexer.token()
+                if not tok: break
+                print tok
+        # save the context
+        save_ctx = ryacc.ctx
+        # put in place our own context
+        ryacc.ctx = self.ctx
+        self.parser.parse(s, lexer=self.lexer)
+        # restore the context back
+        ryacc.ctx = save_ctx
+        return self.parse_result
+
+    
+class ACLRuleParser(Parser):
+    def __init__(self, **kw):
+        self.namespace = kw.get('namespace', { })
+        self.vars = { }
+        self.consts = {
+                'True': True,
+                'False': False,
+                'None': None,
+                }
+        Parser.__init__(self, **kw)
+
+    def get_ns(self):
+        ns ={}
+        ns.update(self.vars)
+        ns.update(self.consts)
+        ns.update(self.namespace)
+        return ns
+
+    def func_acl(self, *args):
+        if len(args) > 1:
+            print "Warning, acl() accepts only one argument"
+        if isinstance(args[0], str):
+            subparser = ACLRuleParser(namespace=copy(self.namespace))
+            return subparser.eval(args[0])
+        else:
+            return args[0]
+
+    def func_split(self, *args):
+        if len(args) > 1 or not isinstance(args[0], str):
+            print "Warning, split() accepts only one string argument"
+        else:
+            return args[0].split()
+
+    def func(self, func, *args):
+        thefunc = getattr(self, 'func_%s' % func, None)
+        if thefunc is None:
+            print "Unknown func %s" % func
+            return False
+        return thefunc(*args)
+
+
+    ########## The parser itself
+
+    tokens = [
+        'NAME',
+        'NUMBER',
+        'COMA',
+        'DOT',
+        'SQCONST',
+        'DQCONST',
+        'PLUS',
+        'MINUS',
+        'EXP',
+        'TIMES',
+        'DIVIDE',
+        'LPAREN',
+        'RPAREN',
+        'EQU',
+        'SUP',
+        'INF',
+        'EQSUP',
+        'EQINF',
+        'DIFF',
+        'NOT',
+        'IN',
+        'AND',
+        'OR',
+        'STARTS',
+        'ENDS',
+        ]
+
+    t_ignore = " \t"
+
+    # Tokens
+    # the following long tokens need whitespaces to be eval'ed before t_NAME
+    t_AND     = r'and                                '
+    t_OR      = r'or                                 '
+    t_NOT     = r'not                                '
+    t_IN      = r'in                                 '
+    t_STARTS  = r':='
+    t_ENDS    = r'=:'
+    t_COMA    = r','
+    t_DOT     = r'\.'
+    t_PLUS    = r'\+'
+    t_MINUS   = r'-'
+    t_EXP     = r'\*\*'
+    t_TIMES   = r'\*'
+    t_DIVIDE  = r'/'
+    t_EQU     = r'=='
+    t_DIFF    = r'!='
+    t_SUP     = r'>'
+    t_INF     = r'<'
+    t_EQSUP   = r'>='
+    t_EQINF   = r'<='
+    t_LPAREN  = r'\('
+    t_RPAREN  = r'\)'
+    t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t_SQCONST = r'\'([^\\\n]|(\\.))*?\''
+    t_DQCONST = r'\"([^\\\n]|(\\.))*?\"'
+
+    precedence = [
+        ('left', 'OR'),
+        ('left', 'AND'),
+        ('right','RNOT'),
+        ('left','EQU','DIFF', 'SUP', 'INF', 'EQSUP', 'EQINF'),
+        ('left','PLUS','MINUS'),
+        ('left','TIMES','DIVIDE'),
+        ('left', 'EXP'),
+        ('right','UMINUS'),
+        ]
+
+    def t_NUMBER(self, t):
+        r"\d+"
+        try:
+            t.value = int(t.value)
+        except ValueError:
+            print "Integer value too large", t.value
+            t.value = False
+        #print "parsed number %s" % repr(t.value)
+        return t
+
+    def t_newline(self, t):
+        r"\n+"
+        t.lineno += t.value.count("\n")
+    
+    def t_error(self, t):
+        print "Illegal character '%s'" % t.value[0]
+        t.skip(1)
+
+    # Parsing rules
+    def p_statement_expr(self, p):
+        """
+        statement : expression
+        """
+        #p[0] = p[1]
+        self.parse_result = p[1]
+
+    def p_expression_callfunc_expr(self, p):
+        """
+        expression : NAME LPAREN expression RPAREN
+        """
+        p[0] = self.func(p[1], p[3])
+
+    def p_expression_callfunc_list(self, p):
+        """
+        expression : NAME LPAREN list RPAREN
+        """
+        p[0] = self.func(p[1], *p[3])
+
+    def p_list_many(self, p):
+        """
+        list : list COMA expression
+        """
+        p[0] = p[1]
+        p[0].append(p[3])
+
+    def p_list_two(self, p):
+        """
+        list : expression COMA expression
+        """
+        p[0] = [ p[1], p[3] ]
+
+    def p_expression_list(self, p):
+        """
+        expression : LPAREN list RPAREN
+        """
+        p[0] = p[2]
+
+    def p_expression_bool(self, p):
+        """
+        expression : expression AND expression
+                  | expression OR expression
+        """
+        #print [repr(p[i]) for i in range(0,4)]
+        if p[2] == 'and':
+            p[0] = p[1] and p[3]
+        elif p[2] == 'or':
+            p[0] = p[1] or p[3]
+
+    def p_expression_binop(self, p):
+        """
+        expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression
+                  | expression EXP expression
+        """
+        #print [repr(p[i]) for i in range(0,4)]
+        try:
+            if p[2] == '+'  : p[0] = p[1] + p[3]
+            elif p[2] == '-': p[0] = p[1] - p[3]
+            elif p[2] == '*': p[0] = p[1] * p[3]
+            elif p[2] == '/': p[0] = p[1] / p[3]
+            elif p[2] == '**': p[0] = p[1] ** p[3]
+        except TypeError:
+            p[0] = False
+
+    def p_expression_test(self, p):
+        """
+        expression : expression EQU expression
+                  | expression DIFF expression
+                  | expression SUP expression
+                  | expression INF expression
+                  | expression EQSUP expression
+                  | expression EQINF expression
+                  | expression STARTS expression
+                  | expression ENDS expression
+                  | expression IN expression
+        """
+        #print [(repr(p[i]), type(p[i])) for i in range(0,4)]
+        try:
+            if p[2] == '=='  : p[0] = p[1] == p[3]
+            elif p[2] == '!='  : p[0] = p[1] != p[3]
+            elif p[2] == '>'  : p[0] = p[1] > p[3]
+            elif p[2] == '<'  : p[0] = p[1] < p[3]
+            elif p[2] == '>='  : p[0] = p[1] >= p[3]
+            elif p[2] == '<='  : p[0] = p[1] <= p[3]
+            elif p[2] == 'in'  : p[0] = p[1] in p[3]
+            elif p[2] == ':='  : p[0] = str(p[1])[:len(p[3])] == p[3]
+            elif p[2] == '=:'  : p[0] = str(p[1])[-len(p[3]):] == p[3]
+        except TypeError:
+            p[0] = False
+
+    def p_expression_uminus(self, p):
+        """
+        expression : MINUS expression %prec UMINUS
+        """
+        try:
+            p[0] = -p[2]
+        except TypeError:
+            p[0] = False
+
+    def p_expression_rnot(self, p):
+        """
+        expression : NOT expression %prec RNOT
+        """
+        p[0] = not p[2]
+
+    def p_expression_group(self, p):
+        """
+        expression : LPAREN expression RPAREN
+        """
+        p[0] = p[2]
+
+    def p_expression_number(self, p):
+        """
+        expression : NUMBER
+        """
+        p[0] = p[1]
+
+    def p_expression_string(self, p):
+        """
+        expression : SQCONST
+                   | DQCONST
+        """
+        q = p[1][0]
+        p[0] = p[1][1:-1].replace('\\'+q, q)
+
+    def p_expression_name(self, p):
+        'expression : NAME'
+        try:
+            p[0] = self.get_ns()[p[1]]
+        except LookupError:
+            print "Undefined name '%s'" % p[1]
+            p[0] = False
+
+    def p_expression_namespace(self, p):
+        'expression : NAME DOT NAME'
+        try:
+            p[0] = self.get_ns()[p[1]][p[3]]
+        except LookupError:
+            #print "Undefined name '%s'" % ''.join(p[1:])
+            p[0] = False
+
+    def p_error(self, p):
+        print "Syntax error at '%s'" % p.value
+
+class String(str):
+    def __init__(self, s):
+        self.string = s[1:-1].replace('\\"', '"')
+        str.__init__(self, self.string)
+
+    def __str__(self):
+        return self.string
+
+class ACLRuleParserInteractive(ACLRuleParser):
+    t_EQUALS  = r'='
+    def __init__(self, **kw):
+        self.tokens.append('EQUALS')
+
+        ACLRuleParser.__init__(self, **kw)
+
+    def p_statement_assign(self, p):
+        """
+        statement : NAME EQUALS expression
+        """
+        if p[1] in self.namespace.keys():
+            print "Cannot reassign a namespace"
+            return
+        self.vars[p[1]] = p[3]
+        self.parse_result = p[3]
+
+
+
+if __name__ == '__main__':
+    ns = {}
+    ns['client'] = {
+            'username': 'foo',
+            'ip_addr': '1.2.3.4',
+            'password': 'bar',
+            'can_connect': 'site.login == "pulp"',
+            }
+    ns['site'] = {
+            'name': 'fiction',
+            'login': 'pulp',
+            'port': '22',
+            'ip_address': '12.23.45.56',
+            }
+    ns['proxy'] = {
+            'time': '03:12',
+            'date': '2006-07-22',
+            'n_clients': '3',
+            }
+
+    if len(sys.argv) > 1:
+        aclrule = ACLRuleParser(debug=0, namespace=ns)
+        print aclrule.eval(' '.join(sys.argv[1:]))
+    else:
+        aclrule = ACLRuleParserInteractive(debug=0, namespace=ns)
+        aclrule.run()
