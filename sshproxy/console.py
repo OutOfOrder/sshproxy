57383e62d65a25ee97e7c5f836ebbb75dabc5524
David Guerizec <david@guerizec.net>
2006-09-01 00:02:51 +0200

    Added exit command in the console

--- sshproxy/console.py
+++ sshproxy/console.py
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2005-2006 David Guerizec <david@guerizec.net>
 #
-# Last modified: 2006 Aug 31, 01:10:07 by david
+# Last modified: 2006 Aug 31, 23:48:59 by david
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -75,6 +75,10 @@ class Console(Registry, cmd.Cmd):
 
     def emptyline(self):
         return
+
+    def do_exit(self, arg):
+        return True
+
 
 Console.register()
 
