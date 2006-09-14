382ba50d3412a19e6b3ef9886b476f4ac10bb5e9
David Guerizec <david@guerizec.net>
2006-09-14 02:16:16 +0200

    Added new aclfunc to get diskspace.
    Usage: diskspace('/mount/point')
    Return value: Used disk space in percent multiplied by 10.
    
    Example ACL:
    
    remote_exec:
      # when /var occupation is over 95%, bob can remove a big file
      client.username == "bob" and diskspace("/var") >= 950
        and proxy.cmdline == "rm -f /var/tmp/big_file"

--- lib/acl_funcs/aclfuncs.py
+++ lib/acl_funcs/aclfuncs.py
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2005-2006 David Guerizec <david@guerizec.net>
 #
-# Last modified: 2006 Aug 07, 02:27:53 by david
+# Last modified: 2006 Sep 14, 01:58:13 by david
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,9 +19,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
+import os
 
 from sshproxy import get_class
 from sshproxy import aclparser
+from sshproxy import log
 
 base = get_class('ACLRuleParser')
 
@@ -29,27 +31,35 @@ class ACLRuleParser(base):
 
     def func_len(self, *args):
         if len(args) != 1:
-            print "Warning: function len takes exactly 1 argument."
+            log.warning("Warning: function len takes exactly 1 argument.")
             return
         return len(args[0])
 
     def func_substr(self, *args):
         if len(args) != 3:
-            print "Warning: function substr takes exactly 3 arguments."
+            log.warning("Warning: function substr takes exactly 3 arguments.")
             return
         start = args[0]
         end = args[1]
         var = args[2]
         return var[start:end]
 
-    def call(self, namespace):
-        # self.right = "start end ns.var"
-        try:
-            start, end, var = self.right.split()
-        except ValueError:
-            return False
-        sub = str(self.left.eval(namespace, Token(str(var))))[int(start):int(end)]
-        return sub
+    def func_diskspace(self, *args):
+        """
+        diskspace("/var") -> 125 (12.5% used)
+        """
+        if len(args) != 1:
+            log.warning("Warning: function diskspace takes exactly 1 argument.")
+            return
+        mountpoint = args[0]
+
+        fd = os.open(mountpoint, os.O_RDONLY)
+        stats = os.fstatvfs(fd)
+        os.close(fd)
+        size = stats[1] * stats[2]
+        free = stats[1] * stats[4]
+        diskspace = 1000 * (size - free) / size
+        return diskspace
 
 ACLRuleParser.register()
 
