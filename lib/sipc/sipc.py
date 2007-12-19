8888717271e625b5fbc9d944132368452e278119
David Guerizec <david@guerizec.net>
2007-12-19 23:38:01 +0100

    Fix issue #6: "sipc server does not check the key from the client"

--- lib/sipc/sipc.py
+++ lib/sipc/sipc.py
@@ -3,7 +3,7 @@
 #
 # Copyright (C) 2005-2007 David Guerizec <david@guerizec.net>
 #
-# Last modified: 2007 Dec 19, 00:47:44 by david
+# Last modified: 2007 Dec 19, 23:21:49 by david
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,22 +19,71 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
-import threading, socket
+import threading, socket, os.path
 import paramiko
 from sshproxy import get_class, log
+from sshproxy.config import ConfigSection, path, get_config
+
+
+class SIPCConfigSection(ConfigSection):
+    section_id = 'sipc'
+    section_defaults = {
+        'authorized_keys': '@authorized_keys',
+        'key_file': '@ipc.id_dsa',
+        }
+    types = {
+        'authorized_keys': path,
+        'key_file': path,
+        }
+
+SIPCConfigSection.register()
 
 class SSHServer(paramiko.ServerInterface):
+    def __init__(self, addr):
+        self.sock_addr = addr
+        paramiko.ServerInterface.__init__(self)
+
     def check_auth_publickey(self, username, key):
-        log.devdebug("SSHServer[IPC]: check_auth_publickey %s %s", username, key)
-        pubkey=key.get_base64()
-        if username == "IPC":
+        pubkey = key.get_base64()
+        addr = self.sock_addr[0]
+
+        if username == "sshproxy-IPC":
+            try:
+                hostkey_file = get_config('sshproxy').get('hostkey_file')
+                hostkey = paramiko.DSSKey(filename=hostkey_file).get_base64()
+
+                auth_keys_file = get_config('sipc')['authorized_keys']
+                if os.path.isfile(auth_keys_file):
+                    authorized_keys = open(auth_keys_file).readlines()
+                else:
+                    authorized_keys = []
+
+                authorized_keys.append(hostkey)
+                if not len([ k for k in authorized_keys if pubkey in k ]):
+                    log.error("ATTENTION: unauthorized attempt to connect "
+                              "on IPC channel from %s@%s" % (username, addr))
+                    return paramiko.AUTH_FAILED
+
+            except:
+                log.exception("SIPC: exception in check_auth_pubkey")
+                return paramiko.AUTH_FAILED
+
+            self.username = username
             return paramiko.AUTH_SUCCESSFUL
+
+        log.error("ATTENTION: unauthorized attempt to connect "
+                  "on IPC channel from %s@%s" % (username, addr))
         return paramiko.AUTH_FAILED
 
     def check_channel_request(self, kind, chanid):
-        log.devdebug("SSHServer[IPC]: check_channel_request %s %s", kind, chanid)
-        if kind == 'IPC':
+        if kind == 'sshproxy-IPC':
             return paramiko.OPEN_SUCCEEDED
+
+        addr = self.sock_addr[0]
+        log.error("ATTENTION: unauthorized attempt to connect "
+                  "on IPC channel type '%s' from %s@%s" %
+                    (kind, self.username, addr))
+
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
 
@@ -59,11 +108,12 @@ class SIPCServer(IPCServer):
         # start the server interface
         negotiation_ev = threading.Event()
 
-        transport.start_server(negotiation_ev, SSHServer())
+        transport.start_server(negotiation_ev, SSHServer(self.sock_addr))
 
         while not negotiation_ev.isSet():
             negotiation_ev.wait(0.5)
         if not transport.is_active():
+            log.error("SIPC: SSH negotiation failed")
             raise 'SSH negotiation failed'
 
         sock = transport.accept(5)
@@ -102,20 +152,26 @@ class SIPCClient(IPCClient):
         while not ev.isSet():
             ev.wait(0.5)
         if not transport.is_active():
+            log.error("SIPC: SSH negotiation failed")
             raise 'SSH negotiation failed'
 
         ev = threading.Event()
 
-        key = paramiko.DSSKey(filename="/etc/sshproxy/id_dsa")
+        key_file = get_config("sipc").get("key_file")
+        if not os.path.isfile(key_file):
+            key_file = get_config("sshproxy").get("hostkey_file")
 
-        transport.auth_publickey('IPC', key, ev)
+        key = paramiko.DSSKey(filename=key_file)
+
+        transport.auth_publickey('sshproxy-IPC', key, ev)
 
         while not ev.isSet():
             ev.wait(0.5)
         if not transport.is_authenticated():
+            log.error("SIPC: SSH authentication failed")
             raise 'SSH authentication failed'
 
-        sock = transport.open_channel('IPC') #, dest_addr=sock_addr, src_addr=None)
+        sock = transport.open_channel('sshproxy-IPC')
         self.real_sock = real_sock
         self.transport = transport
 
